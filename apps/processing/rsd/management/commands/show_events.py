5b349101c0b427c6088db4101a08f73b2abe9fc3
etomanon <etomanon@gmail.com>
2018-03-21 13:48:12 +0100

    show rsd events fix

--- apps/processing/rsd/management/commands/show_events.py
+++ apps/processing/rsd/management/commands/show_events.py
@@ -11,19 +11,17 @@ from apps.utils.time import UTC_P0100
 from psycopg2.extras import DateTimeTZRange
 from django.contrib.gis.geos import GEOSGeometry
 
+
 class Command(BaseCommand):
     help = 'Find unique events of given categories that' \
             'occur at given time range and in given spatial extent.'
 
-# ./dcmanage.sh show_events --geometry 'POINT(16.597252 49.157379)' --categories ['Práce na silnici','Nehoda'] --time_range_start "1 1 2018 12:00" --time_range_end "28 1 2018 20:00" --buffer 10000
-
-
-
+# ./dcmanage.sh show_events --geometry 'POINT(16.597252 49.157379)' --categories 'Nehoda, 456' --time_range_start "1 1 2018 12:00" --time_range_end "28 1 2018 20:00" --buffer 10000
 
     def add_arguments(self, parser):
         parser.add_argument('--geometry', nargs='?', type=str,
                             default=None)
-        parser.add_argument('--categories', nargs='?', type=list,
+        parser.add_argument('--categories', nargs='?', type=str,
                             default=None)
         parser.add_argument('--time_range_start', nargs='?', type=str,
                             default=None)
@@ -38,80 +36,73 @@ class Command(BaseCommand):
         time_range_start = options['time_range_start']
         time_range_end = options['time_range_end']
         buffer = options['buffer']
-        if geom is None:
-            raise Exception("No geometry defined!")
-        else:
-            geom = GEOSGeometry(geom, srid=4326)
-            geom = geom.transform(3857,clone=True)
-        categories = []
-        if categories_param is None:
-            raise Exception("No event categories defined!")
-        else:
-            text = ''
-            for category in categories_param:
-                if(category == '['):
-                    continue
-                if(category == ']'):
-                    categories.append(text)
-                    continue
-                if(category == ','):
-                    categories.append(text)
-                    text = ''
-                    continue
-                text += category
+        findEvents(geom, categories_param, time_range_start, time_range_end, buffer)
 
-        if time_range_start is None or time_range_end is None:
-            raise Exception("No time range defined!")
-        else:
-            start_range = parse(time_range_start)
-            end_range = parse(time_range_end)
-            if(start_range.tzinfo is None or start_range.tzinfo.utcoffset(start_range) is None):
-                
-                start_range = start_range.replace(tzinfo=UTC_P0100)
-            if(end_range.tzinfo is None or end_range.tzinfo.utcoffset(end_range) is None):
-                end_range = end_range.replace(tzinfo=UTC_P0100)
-            if(start_range.tzinfo != UTC_P0100):
-                start_range = start_range.astimezone(UTC_P0100)
-            if(end_range.tzinfo != UTC_P0100):
-                end_range = end_range.astimezone(UTC_P0100)
 
-        if buffer is None:
-            raise Exception("No buffer defined!")
-        else:
-            buffer = buffer
-
-        geom_final = geom.buffer(buffer)
-
-        i = 0
-        result = []
-        for event in EventObservation.objects.iterator():
-            is_geometry = False
-            is_category = False
-            is_time = False
+def findEvents(geom, categories_param, time_range_start, time_range_end, buffer):
+    if geom is None:
+        raise Exception("No geometry defined!")
+    else:
+        geom = GEOSGeometry(geom, srid=4326)
+        geom = geom.transform(3857,clone=True)
+    categories = []
+    if categories_param is None:
+        raise Exception("No event categories defined!")
+    else:
+        categories = categories_param.split(",")   
+    if time_range_start is None or time_range_end is None:
+        raise Exception("No time range defined!")
+    else:
+        start_range = parse(time_range_start)
+        end_range = parse(time_range_end)
+        if(start_range.tzinfo is None or start_range.tzinfo.utcoffset(start_range) is None):
             
-            for category in categories:
-                if(category == event.category.name):
-                    is_category = True
-            if(not is_category):
-                continue
+            start_range = start_range.replace(tzinfo=UTC_P0100)
+        if(end_range.tzinfo is None or end_range.tzinfo.utcoffset(end_range) is None):
+            end_range = end_range.replace(tzinfo=UTC_P0100)
+        if(start_range.tzinfo != UTC_P0100):
+            start_range = start_range.astimezone(UTC_P0100)
+        if(end_range.tzinfo != UTC_P0100):
+            end_range = end_range.astimezone(UTC_P0100)
 
-            start_time = event.phenomenon_time_range.lower
-            end_time = event.phenomenon_time_range.upper
+    if buffer is None:
+        raise Exception("No buffer defined!")
+    else:
+        buffer = buffer
 
-            is_time = time_in_range(start_time, end_time, start_range, end_range)
-            if(not is_time):
-                continue
+    geom_final = geom.buffer(buffer)
 
-            for admin_unit in event.result.admin_units.iterator():
-                if(admin_unit.geometry.intersects(geom_final)):
-                    is_geometry = True
-                    # print('Intersected: {}'.format(admin_unit))
+    i = 0
+    result = []
+    for event in EventObservation.objects.iterator():
+        is_geometry = False
+        is_category = False
+        is_time = False
+        
+        for category in categories:
+            if(category == event.category.id_by_provider or category == event.category.name):
+                is_category = True
+        if(not is_category):
+            continue
 
-            if(is_geometry):
-                i += 1
-                print('Number of EventObservations: {}'.format(i))
-                result.append(event)
-        print('Result: {}'.format(result))
+        start_time = event.phenomenon_time_range.lower
+        end_time = event.phenomenon_time_range.upper
+
+        is_time = time_in_range(start_time, end_time, start_range, end_range)
+        if(not is_time):
+            continue
+
+        for admin_unit in event.result.admin_units.iterator():
+            if(admin_unit.geometry.intersects(geom_final)):
+                is_geometry = True
+                # print('Intersected: {}'.format(admin_unit))
+
+        if(is_geometry):
+            i += 1
+            print('Number of EventObservations: {}'.format(i))
+            result.append(event)
+    print('Result: {}'.format(result))
+
 
 def time_in_range(start, end, day_start, day_end):
     """Return true if event time range overlaps time range in argument"""
