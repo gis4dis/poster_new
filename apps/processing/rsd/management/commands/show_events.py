b2d2f7970036edd10dc6704a87074f8da5bd634d
etomanon <etomanon@gmail.com>
2018-04-11 12:31:08 +0200

    rsd find events fix

--- apps/processing/rsd/management/commands/show_events.py
+++ apps/processing/rsd/management/commands/show_events.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
     help = 'Find unique events of given categories that' \
             'occur at given time range and in given spatial extent.'
 
-# ./dcmanage.sh show_events --geometry 'POINT(16.597252 49.157379)' --categories '201,211' --time_range_start "1 1 2018 12:00" --time_range_end "28 1 2018 20:00" --buffer 10000
+# ./dcmanage.sh show_events --geometry 'POINT(16.597252 49.157379)' --categories 'Nehody,211' --time_range_start "1 1 2018 12:00" --time_range_end "28 1 2018 20:00" --buffer 10000
 
     def add_arguments(self, parser):
         parser.add_argument('--geometry', nargs='?', type=str,
@@ -36,66 +36,55 @@ class Command(BaseCommand):
         time_range_start = options['time_range_start']
         time_range_end = options['time_range_end']
         buffer = options['buffer']
-        findEvents(geom, categories_param, time_range_start, time_range_end, buffer)
+
+        if geom is None:
+            raise Exception("No geometry defined!")
+        else:
+            geom = GEOSGeometry(geom, srid=4326)
+            geom = geom.transform(3857,clone=True)
+        
+        if time_range_start is None or time_range_end is None:
+            raise Exception("No time range defined!")
+        else:
+            start_range = parse(time_range_start)
+            end_range = parse(time_range_end)
+            if(start_range.tzinfo is None or start_range.tzinfo.utcoffset(start_range) is None):
+                
+                start_range = start_range.replace(tzinfo=UTC_P0100)
+            if(end_range.tzinfo is None or end_range.tzinfo.utcoffset(end_range) is None):
+                end_range = end_range.replace(tzinfo=UTC_P0100)
+            if(start_range.tzinfo != UTC_P0100):
+                start_range = start_range.astimezone(UTC_P0100)
+            if(end_range.tzinfo != UTC_P0100):
+                end_range = end_range.astimezone(UTC_P0100)
+        if buffer is None:
+            raise Exception("No buffer defined!")
+        if categories_param is None:
+            raise Exception("No event categories defined!")
+        else:
+            categories = categories_param.split(",")
+        findEvents(geom, categories, start_range, end_range, buffer)
 
 
-def findEvents(geom, categories_param, time_range_start, time_range_end, buffer):
-    if geom is None:
-        raise Exception("No geometry defined!")
-    else:
-        geom = GEOSGeometry(geom, srid=4326)
-        geom = geom.transform(3857,clone=True)
-    categories = []
-    if categories_param is None:
-        raise Exception("No event categories defined!")
-    else:
-        categories = categories_param.split(",")  
-        find_by_id = False
-        find_by_group = False
-        for category in categories:
-            is_digit = category.isdigit()
-            if(not is_digit):
-                find_by_group = True
-            if(is_digit):
-                find_by_id = True
-            if(find_by_id and find_by_group):
-                raise Exception("Categories must be strings or integers only - no mix of strings and integers allowed!")
-    if time_range_start is None or time_range_end is None:
-        raise Exception("No time range defined!")
-    else:
-        start_range = parse(time_range_start)
-        end_range = parse(time_range_end)
-        if(start_range.tzinfo is None or start_range.tzinfo.utcoffset(start_range) is None):
-            
-            start_range = start_range.replace(tzinfo=UTC_P0100)
-        if(end_range.tzinfo is None or end_range.tzinfo.utcoffset(end_range) is None):
-            end_range = end_range.replace(tzinfo=UTC_P0100)
-        if(start_range.tzinfo != UTC_P0100):
-            start_range = start_range.astimezone(UTC_P0100)
-        if(end_range.tzinfo != UTC_P0100):
-            end_range = end_range.astimezone(UTC_P0100)
-
-    if buffer is None:
-        raise Exception("No buffer defined!")
-    else:
-        buffer = buffer
-
+def findEvents(geom, categories, start_range, end_range, buffer):
+    categories_group = []
+    categories_id = []
+    for category in categories:
+        is_digit = category.isdigit()
+        if(not is_digit):
+            categories_group.append(category)
+        if(is_digit):
+            categories_id.append(category)
     geom_final = geom.buffer(buffer)
 
     i = 0
     result = []
     pt_range = DateTimeTZRange(start_range, end_range)
-    if(find_by_group):
-        evt_obj = EventObservation.objects.filter(
-            phenomenon_time_range__overlap=pt_range,
-            category__group__in=categories
-            )
-    else:
-        evt_obj = EventObservation.objects.filter(
-            phenomenon_time_range__overlap=pt_range,
-            category__id_by_provider__in=categories
-            )
-    
+    evt_obj = EventObservation.objects.filter(
+        phenomenon_time_range__overlap=pt_range,
+        category__group__in=categories_group,
+        )
+
     for event in evt_obj:
         is_geometry = False
 
@@ -108,6 +97,33 @@ def findEvents(geom, categories_param, time_range_start, time_range_end, buffer)
             i += 1
             print('Number of EventObservations: {}'.format(i))
             result.append(event)
+
+    ids = []
+    for event in result:
+        ids.append(event.id_by_provider)
+        
+    evt_obj = (EventObservation.objects.filter(
+        phenomenon_time_range__overlap=pt_range,
+        category__id_by_provider__in=categories_id
+        ))
+    
+    for event in evt_obj:
+        if(event.id_by_provider in ids):
+            continue
+
+        is_geometry = False
+        for admin_unit in event.result.admin_units.iterator():
+            if(admin_unit.geometry.intersects(geom_final)):
+                is_geometry = True
+                # print('Intersected: {}'.format(admin_unit))
+        
+        if(is_geometry):
+            i += 1
+            print('Number of EventObservations: {}'.format(i))
+            result.append(event)
+
     print('Result: {}'.format(result))
+    print('Number of EventObservations: {}'.format(i))
+    return result
 
             
