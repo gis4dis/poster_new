ebff022dea65ed3a484a38c388551a964956333b
Ludvik Adamec <ja@ludvikadamec.cz>
2018-04-24 10:15:38 +0200

    pmo - watercourse station import done

--- apps/processing/pmo/management/commands/import_stations.py
+++ apps/processing/pmo/management/commands/import_stations.py
@@ -1,18 +1,13 @@
 import logging
 from django.core.management.base import BaseCommand
-from apps.processing.ala.util import util
-from dateutil.parser import parse
-from dateutil import relativedelta
-from datetime import date, timedelta
 from django.core.files.storage import default_storage
-import os
 import csv
 from apps.processing.pmo.models import MeteoStation
 from django.contrib.gis.geos import GEOSGeometry, Point
 import io
+import re
 
 logger = logging.getLogger(__name__)
-import re
 
 
 def parse_dms(dms):
@@ -28,27 +23,27 @@ def parse_dms(dms):
     return degree, minute, second, ms
 
 
+#Logovani - mam do loggeru posilat info o zalozeni stanice nebo update?
+#   co by vsechno melo byt logovane?
+#Celery task ???? Jak?
+
 class Command(BaseCommand):
     help = 'Import stations '
 
     def add_arguments(self, parser):
         parser.add_argument('--path', nargs='?', type=str,
-                            default=None)
+                            default='/pmo/stanice_tok.csv')
 
     def handle(self, *args, **options):
         arg = options['path']
+
         if arg is None:
-            raise Exception("No path to file defined!")
+            logger.info('No path to file defined! Importing from default path.')
         else:
             path = arg
 
-            folder = '/pmo/'
-            file_name = 'stanice_tok.csv'
-
-            if default_storage.exists(folder + file_name):
-                print('exists')
-
-                csv_file = default_storage.open(name=(folder + file_name), mode='rt')
+            if default_storage.exists(path):
+                csv_file = default_storage.open(name=path, mode='r')
                 foo = csv_file.data.decode('utf-8')
                 reader = csv.reader(io.StringIO(foo))
 
@@ -58,6 +53,7 @@ class Command(BaseCommand):
                 for rid_x, row in enumerate(rows, 1):
                     id_by_prov = row[0]
                     name = row[1]
+                    watercourse = row[2]
 
                     (degree, minute, second, ms) = parse_dms(row[3])
                     lat = (int(degree) + float(minute) / 60 + float(second) / 3600 + float(ms) / 36000)
@@ -73,13 +69,18 @@ class Command(BaseCommand):
                         geom = GEOSGeometry(geom, srid=4326)
                         geom = geom.transform(3857, clone=True)
 
-                        obs = MeteoStation.objects.create(
-                            id_by_provider=id_by_prov,
-                            name=name,
-                            geometry=geom
-                        )
+                        defaults = {
+                            'id_by_provider': id_by_prov,
+                            'name': name,
+                            'geometry': geom,
+                            'watercourse': watercourse
+                        }
 
-                        print(obs)
+                        #logger???
+                        MeteoStation.objects.update_or_create(
+                            id_by_provider=id_by_prov,
+                            defaults=defaults
+                        )
             else:
-                print('notExists')
+                logger.warning("Error specified path: %s not found", path)
 
--- apps/processing/pmo/models.py
+++ apps/processing/pmo/models.py
@@ -4,8 +4,14 @@ from django.contrib.gis.db import models
 
 class MeteoStation(AbstractFeature):
     # Ala doesn't have geometry value to the yet - to be added in future
+
     geometry = models.PointField(
         help_text="Spatial information about station.",
         srid=3857
+    )
+
+    watercourse = models.CharField(
+        help_text="Human-readable name of the watercourse.",
+        max_length=150
     )
 
