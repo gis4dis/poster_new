1f302049165be1f016ecc472d0e540d417312cc7
etomanon <etomanon@gmail.com>
2018-05-09 22:53:41 +0200

    moved ala props_def, processes_def to common

--- apps/processing/rsd/management/commands/import_observed.py
+++ apps/processing/rsd/management/commands/import_observed.py
@@ -1,4 +1,6 @@
 from django.db import models
+from apps.common.util.util import processes_def, props_def
+from apps.utils.obj import *
 from apps.common.models import Property, Process
 from django.core.management.base import BaseCommand
 
@@ -7,25 +9,24 @@ class Command(BaseCommand):
     help = 'Create observed property & process for events'
 
     def handle(self, *args, **options):
-        try:
-            prop = Property.objects.get(name_id='occuring_events')
-            prop.name="occuring events"
-            prop.save()
-        except:
-            prop = Property(
-                name_id='occuring_events',
-                name='occuring events',
-                unit=''
-                )
-            prop.save()
-        try:
-            proc = Process.objects.get(name_id='observation')
-            proc.name="observation"
-            proc.save()
-        except:
-            proc = Process(
-                name_id='observation',
-                name='observation',
-                )
-            proc.save()
+        get_or_create_processes()
+        get_or_create_props()
         print('Metadata for EventObservations imported')
+
+
+def get_or_create_props():
+    for prop in props_def:
+        if 'default_mean' in prop[1]:
+            mean = prop[1]['default_mean']
+            if not isinstance(prop[1]['default_mean'], Process):
+                mean_process = Process.objects.get(name_id=mean)
+                if mean and mean_process:
+                    prop[1]['default_mean'] = mean_process
+                else:
+                    prop[1]['default_mean'] = None
+
+    return get_or_create_objs(Property, props_def, 'name_id')
+
+
+def get_or_create_processes():
+    return get_or_create_objs(Process, processes_def, 'name_id')
