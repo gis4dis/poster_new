1989da269efc1163fe63cf45c44fb1a0b21ab25a
SLeitgeb <leitgeb.simon@gmail.com>
2018-05-29 19:16:35 +0200

    Changes to get_timeseries function arguments as per MC–AD interface specification

--- apps/ad/anomaly_detection.py
+++ apps/ad/anomaly_detection.py
@@ -1,57 +1,37 @@
-import logging
-from django.core.management.base import BaseCommand
 from django.conf import settings
-from apps.processing.ala.util import util
-from psycopg2.extras import DateTimeTZRange
-from dateutil.parser import parse
-from dateutil import relativedelta
-from datetime import date, timedelta
-from apps.common.models import Process, Property
-from apps.processing.ala.models import SamplingFeature, Observation
-from luminol.anomaly_detector import AnomalyDetector
-from datetime import datetime
+from apps.common.models import Process
+
 import pytz
 import time
+from psycopg2.extras import DateTimeTZRange
+from dateutil.parser import parse
 
-logger = logging.getLogger(__name__)
+from luminol.anomaly_detector import AnomalyDetector
 
 stations_def = ('11359201', '11359196', '11359205',
                 '11359192', '11359202', '11359132')
 
 
-def get_timeseries(prop="air_temperature", observation_provider_model="apps.processing.ala.Observation", feature_of_interest='11359201', time_range=DateTimeTZRange("2018-01-01 00:00:00+01", "2019-01-01 00:00:00+01")):
-    time_range_field = "phenomenon_time_range"
-
-    proc = settings.APPLICATION_MC.PROPERTIES[prop]["process"]
-    frequency = settings.APPLICATION_MC.PROPERTIES[prop]["value_frequency"]
-    feature = SamplingFeature.objects.get(id_by_provider=feature_of_interest)
-    observed_property = Property.objects.get(name_id=prop)
-    procedure = Process.objects.get(name_id=proc)
+def get_timeseries(observed_property, observation_provider_model, feature_of_interest, phenomenon_time_range=DateTimeTZRange("2018-01-01 00:00:00+01", "2019-01-01 00:00:00+01")):
+    frequency = settings.APPLICATION_MC.PROPERTIES[observed_property.name_id]["value_frequency"]
+    process = Process.objects.get(
+        name_id=settings.APPLICATION_MC.PROPERTIES[observed_property.name_id]["process"])
+    observation_model_name = f"{observation_provider_model.__module__}.{observation_provider_model.__name__}"
     
-    print(observed_property, procedure, feature)
+    print(observed_property, process, feature_of_interest)
 
-    obss = Observation.objects.raw('''SELECT * FROM ala_observation WHERE
+    obss = observation_provider_model.objects.raw('''SELECT * FROM ala_observation WHERE
         lower(phenomenon_time_range) BETWEEN %s AND %s AND
         mod(cast(extract(epoch from lower(phenomenon_time_range)) as int), %s)=0 AND
         mod(cast(extract(epoch from upper(phenomenon_time_range)) as int) - cast(extract(epoch from lower(phenomenon_time_range)) as int), %s)=0 AND
         observed_property_id=%s AND
         procedure_id=%s AND
-        feature_of_interest_id=%s''', [time_range.lower, time_range.upper, frequency, frequency, observed_property.id, procedure.id, feature.id])
-
-    # obss = Observation.objects.filter(
-    #     # phenomenon_time_range__starts_with=F("phenomenon_time_range")[
-    #     #     0] - F("phenomenon_time_range")[0] % frequency,
-    #     # phenomenon_time_range__ends_with=F("phenomenon_time_range")[1] - (
-    #     #     F("phenomenon_time_range")[1] - F("phenomenon_time_range")[0]) % frequency,
-    #     feature_of_interest=feature_of_interest,
-    #     observed_property=Property.objects.get(name_id=prop),
-    #     procedure=Process.objects.get(name_id=procedure)
-    # )
+        feature_of_interest_id=%s''', [phenomenon_time_range.lower, phenomenon_time_range.upper, frequency, frequency, observed_property.id, process.id, feature_of_interest.id])
 
     anomalyScore = anomaly_detect(obss)
 
     result = {
-        'phenomenon_time_range': time_range,
+        'phenomenon_time_range': phenomenon_time_range,
         'value_frequency': frequency,
         'property_values': obss,
         'property_anomaly_rates': anomalyScore,
@@ -63,7 +43,7 @@ def anomaly_detect(list_obss, detector_method='default_detector'):
     results_dic = dict()
     for line in list_obss:
         results_dic[time.mktime(line.phenomenon_time_range.lower.astimezone(
-            pytz.utc).timetuple())] = float(line.result)
+            pytz.utc).timetuple())] = None if line.result == None else float(line.result)
 
     my_detector = AnomalyDetector(
         results_dic, None, False, None, None, detector_method, None, None, None)
