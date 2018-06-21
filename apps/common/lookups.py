399d1f052d7ee13630905f8323df1906145c0754
Ludvik Adamec <ja@ludvikadamec.cz>
2018-06-22 00:04:58 +0200

    Preparing new admin  duration filter

--- apps/common/lookups.py
+++ apps/common/lookups.py
@@ -1,5 +1,6 @@
 from django.db.models import Lookup
 from django.contrib.postgres.fields import DateTimeRangeField
+from psycopg2.extras import NumericRange
 
 @DateTimeRangeField.register_lookup
 class Duration(Lookup):
@@ -9,6 +10,12 @@ class Duration(Lookup):
     lhs, lhs_params = self.process_lhs(compiler, connection)
     rhs, rhs_params = self.process_rhs(compiler, connection)
     params = lhs_params + rhs_params
+
+    if type(rhs_params[0]) is NumericRange:
+        params = lhs_params + [rhs_params[0].lower] + lhs_params + [rhs_params[0].upper]
+        return "duration(%s) >= (%s || ' second')::interval AND duration(%s) < (%s || ' second')::interval" % \
+               (lhs, rhs, lhs, rhs), params
+
     return "duration(%s) = (%s || ' second')::interval" % (lhs, rhs), params
 
 
