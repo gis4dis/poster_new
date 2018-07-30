44f5d9838121f15c8b2f6fbaace558c19626ebce
Ludvik Adamec <ja@ludvikadamec.cz>
2018-07-30 11:02:57 +0200

    Unit tests for get_timeseries done

--- apps/processing/ala/tests.py
+++ apps/processing/ala/tests.py
@@ -26,16 +26,19 @@ from apps.ad.anomaly_detection import get_timeseries
 
 '''
 
+time_range_boundary = '[)'
+time_from = datetime(2018, 6, 15, 00, 00, 00)
+date_time_range = DateTimeTZRange(
+    time_from,
+    time_from + timedelta(hours=24),
+    time_range_boundary
+)
+
 
 def get_time_series_test():
     station = SamplingFeature.objects.get(name="Brno")
     prop = Property.objects.get(name_id='air_temperature')
-    time_from = datetime(2018, 6, 15, 00, 00, 00)
-    time_range = DateTimeTZRange(
-        time_from,
-        time_from + timedelta(hours=24),
-        time_range_boundary
-    )
+    time_range = date_time_range
 
     return get_timeseries(
         observed_property=prop,
@@ -44,7 +47,6 @@ def get_time_series_test():
         phenomenon_time_range=time_range
     )
 
-time_range_boundary = '[)'
 
 class SamplingFeatureTestCase(TestCase):
     def setUp(self):
@@ -161,9 +163,68 @@ class SamplingFeatureTestCase(TestCase):
         ts = get_time_series_test()
         self.assertEqual(len(ts['property_values']), len(ts['property_anomaly_rates']))
 
+    def test_time_series_out_bounds(self):
+        ts = get_time_series_test()
+        lower_inc = ts['phenomenon_time_range'].lower_inc
+        upper_inc = ts['phenomenon_time_range'].upper_inc
+        self.assertTrue(lower_inc)
+        self.assertFalse(upper_inc)
 
-    '''
-    def test_sampling_feature_exists_fail(self):
-        station = SamplingFeature.objects.get(name="Brno")
-        self.assertEqual(station.name, 'BrnoFail')
+    def test_time_series_out_lower_is_multiply_of_value_frequency(self):
+        ts = get_time_series_test()
+        result = ts['phenomenon_time_range'].lower.timestamp()
+        f = ts['value_frequency']
+        self.assertEqual(result % f, 0)
+
+    def test_time_series_out_interval_is_multiply_of_value_frequency(self):
+        ts = get_time_series_test()
+        lower = ts['phenomenon_time_range'].lower
+        upper = ts['phenomenon_time_range'].upper
+        result = upper.timestamp() - lower.timestamp()
+        f = ts['value_frequency']
+        self.assertEqual(result % f, 0)
+
+    def test_time_series_time_range_in_contains_out(self):
+        ts = get_time_series_test()
+        out_lower = ts['phenomenon_time_range'].lower
+        out_upper = ts['phenomenon_time_range'].upper
+        self.assertTrue(out_lower >= date_time_range.lower)
+        self.assertTrue(out_upper < date_time_range.upper)
+
+    def test_time_series_in_bounds(self):
+        ts = get_time_series_test()
+        lower_inc = date_time_range.lower_inc
+        upper_inc = date_time_range.upper_inc
+        self.assertTrue(lower_inc)
+        self.assertFalse(upper_inc)
+
+    def test_time_series_in_lower_is_multiply_of_value_frequency(self):
+        ts = get_time_series_test()
+        result = date_time_range.lower.timestamp()
+        f = ts['value_frequency']
+        self.assertEqual(result % f, 0)
+
+    def test_time_series_in_interval_is_multiply_of_value_frequency(self):
+        ts = get_time_series_test()
+        lower = date_time_range.lower
+        upper = date_time_range.upper
+        result = upper.timestamp() - lower.timestamp()
+        f = ts['value_frequency']
+        self.assertEqual(result % f, 0)
+
+    def test_property_values_length_equals_out_range(self):
+        ts = get_time_series_test()
+        lower = ts['phenomenon_time_range'].lower
+        upper = ts['phenomenon_time_range'].upper
+        property_values_length = len(ts['property_values'])
+        f = ts['value_frequency']
+        estimated_length = (upper.timestamp() - lower.timestamp()) / f
+        self.assertEquals(property_values_length, estimated_length)
+
+    '''    
+    TODO
+    observation.phenomenon_time_range.lower.timestamp() % value_frequency == 0
+
+    TODO
+    (observation.phenomenon_time_range.upper.timestamp() - observation.phenomenon_time_range.lower.timestamp()) == value_frequency
     '''
