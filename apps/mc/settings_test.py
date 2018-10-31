c606c5d083e2dd6cac343cd851b5fa12199d79db
Ludvik Adamec <ja@ludvikadamec.cz>
2018-10-31 10:16:32 +0100

    Aggregate observations tests done, Test settings_test.py added

---
+++ apps/mc/settings_test.py
@@ -0,0 +1,132 @@
+# mandatory, dictionary, at least one topic must be specified
+TOPICS = {
+    # common.Topic.name_id, dictionary, at least one property must be specified
+    # topic represents an emergency situation, e.g. drought or floods
+    'drought': {
+
+        # dictionary of properties related to the topic
+        # mandatory, at least one property must be specified
+        'properties': {
+
+            # common.Property.name_id
+            'air_temperature': {
+
+                # dictionary of observation providers of given property
+                # mandatory, at least one provider must be specified
+                'observation_providers': {
+
+                    # path to Django model
+                    # the model must be subclass of common.AbstractObservation
+                    'apps.processing.ala.models.Observation': {
+
+                        # mandatory, name_id of common.Process
+                        'process': 'apps.common.aggregate.arithmetic_mean'
+                    },
+
+                    'apps.processing.ozp.models.Observation': {
+                        'process': 'measure'
+                    }
+                },
+            },
+            'precipitation': {
+                'observation_providers': {
+                    'apps.processing.ala.models.Observation': {
+                        'process': 'apps.common.aggregate.arithmetic_mean',
+                    },
+                }
+            },
+            'pm10': {
+                'observation_providers': {
+                    'apps.processing.ozp.models.Observation': {
+                        'process': 'measure',
+                    },
+                }
+            },
+            'stream_flow': {
+                'observation_providers': {
+                    'apps.processing.pmo.models.WatercourseObservation': {
+                        'process': 'apps.common.aggregate.arithmetic_mean',
+                    },
+                }
+            }
+        },
+
+        # mandatory, number of seconds
+        'value_frequency': 3600
+    },
+    'floods': {
+
+        # dictionary of properties related to the topic
+        # mandatory, at least one property must be specified
+        'properties': {
+
+            # common.Property.name_id
+            'air_temperature': {
+
+                # dictionary of observation providers of given property
+                # mandatory, at least one provider must be specified
+                'observation_providers': {
+
+                    # path to Django model
+                    # the model must be subclass of common.AbstractObservation
+                    'apps.processing.ala.models.Observation': {
+
+                        # mandatory, name_id of common.Process
+                        'process': 'apps.common.aggregate.arithmetic_mean'
+                    },
+                    'apps.processing.ozp.models.Observation': {
+                        'process': 'measure'
+                    }
+                },
+            },
+            'ground_air_temperature': {
+                'observation_providers': {
+                    'apps.processing.ala.models.Observation': {
+                        'process': 'apps.common.aggregate.arithmetic_mean',
+                    },
+                }
+            }
+        },
+
+        # mandatory, number of seconds
+        'value_frequency': 3600
+    },
+
+    # ...
+}
+
+AGGREGATED_OBSERVATIONS = [
+
+    # dictionary representing set of aggregated observations
+    {
+
+        # mandatory, definition of common.TimeSeries
+        'time_series': {
+
+            # datetime in ISO 8601, see https://en.wikipedia.org/wiki/ISO_8601
+            'zero': '2018-09-19T00:00:00+01:00',
+
+            # time interval in ISO 8601 "format with designators", see https://en.wikipedia.org/wiki/ISO_8601#Durations
+            'frequency': 'PT1H',
+            'range_from': 'PT0S',
+            'range_to': 'PT1H',
+        },
+
+        # dictionary of observation providers
+        # mandatory, at least one provider must be specified
+        'observation_providers': {
+
+            # path to Django model
+            # the model must be subclass of common.AbstractObservation
+            'apps.processing.ala.models.Observation': {
+
+                # mandatory, name_id of common.Process
+                'process': 'measure',
+
+                # mandatory, list of name_ids of common.Property
+                'observed_properties': ['precipitation', 'air_temperature',
+                                        'ground_air_temperature'],
+            },
+        },
+    },
+]
