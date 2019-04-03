a4fa4c9bf77b452873c560205d5d69d0eb261c35
Ludvik Adamec <ja@ludvikadamec.cz>
2019-04-03 13:08:27 +0200

    Aggregate - sync celery task execution

--- apps/mc/management/commands/aggregate_observations.py
+++ apps/mc/management/commands/aggregate_observations.py
@@ -1,6 +1,9 @@
 import logging
 from django.core.management.base import BaseCommand
+from celery import chain
+
 logger = logging.getLogger(__name__)
+
 from apps.mc.tasks import compute_aggregated_values, import_time_slots_from_config
 
 
@@ -11,6 +14,5 @@ class Command(BaseCommand):
         parser.add_argument('aggregate_updated_since', nargs='?', default=None)
 
     def handle(self, *args, **options):
-        import_time_slots_from_config.apply_async()
-        compute_aggregated_values.apply_async(kwargs={'aggregate_updated_since_datetime':(options['aggregate_updated_since'])})
-        #compute_aggregated_values()
+        chain(import_time_slots_from_config.si() |
+              compute_aggregated_values.si(aggregate_updated_since_datetime=options['aggregate_updated_since']))()
